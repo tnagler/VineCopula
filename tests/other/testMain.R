0a3ab4cd7a8613d1731e9db6f0a448a784465db8
Ulf <schepsme@ma.tum.de>
2015-09-24 16:16:10 +0200

    New tests for BiCopCDF and BiCopPDF

--- tests/other/testMain.R
+++ tests/other/testMain.R
@@ -43,12 +43,49 @@ if(!all(check_BiCopPar2TailDep)){
 }
 
 
-# testRunBiCopTau2Par
+# BiCopTau2Par
 results_BiCopTau2Par <- testRunBiCopTau("BiCopTau2Par")
 check_BiCopTau2Par <- testCheck2(results_BiCopTau2Par)
 if(!all(check_BiCopTau2Par)){
   print(check_BiCopTau2Par)
 } else {
   rm(results_BiCopTau2Par)
+  gc()
+}
+
+
+# BiCopCDF
+system.time({
+  results_BiCopCDF <- testRunBiCop("BiCopCDF")
+})
+## Gauss alone
+#User      System verstrichen 
+#93.73        0.00       94.22
+
+## All families exept rotated ones, i.e. 11 families
+#User      System verstrichen 
+#116.06        1.27      147.56
+
+## But memory is at 2.5 GB for these 11 families!
+
+check_BiCopCDF <- testCheck3(results_BiCopCDF)
+if(!all(check_BiCopCDF)){
+  print(check_BiCopCDF)
+} else {
+  rm(results_BiCopCDF)
+  gc()
+}
+
+
+# BiCopPDF
+system.time({
+  results_BiCopPDF <- testRunBiCop("BiCopPDF")
+})
+
+check_BiCopPDF <- testCheck3(results_BiCopPDF)
+if(!all(check_BiCopPDF)){
+  print(check_BiCopPDF)
+} else {
+  rm(results_BiCopPDF)
   gc()
 }
--- tests/other/testRun.R
+++ tests/other/testRun.R
@@ -158,3 +158,113 @@ testRunBiCopTau <- function(FUN){
   
   return(results)
 }
+
+
+
+testRunBiCop <- function(FUN){
+  ## familyset
+  #familyset <- c(1:10,13:20,23:30,33:40,104,114,124,134,204,214,224,234)
+  familyset <- c(1:10, 104, 204)
+  #familyset <- familyset[-which(familyset %in% c(15,25,35))]
+  
+  if(FUN == "BiCopCDF") familyset <- familyset[-which(familyset == 2)]
+  
+  ## parameter sets
+  parset3 <- seq(0, 0.99, 0.01)
+  parset3a <- seq(1, 1.99, 0.01)
+  parset1 <- c(parset3, seq(1, 10, 0.25))
+  parset2 <- c(parset3a, seq(2, 10, 0.25))
+  parset4 <- seq(2, 30, 2)
+  
+  ## copula data
+  u1 <- c(seq(0.001,0.01,0.002), seq(0.01,0.99,0.02), seq(0.99,0.999,0.002))
+  u2 <- u1
+  
+  ## return the results in a list
+  results <- list()
+  
+  k <- 1
+  for(fam in familyset){  # run over all families
+    ## set the correct parameter set
+    if(fam == 1){
+      res <- array(0, dim=c(length(parset3), length(u1), length(u2)))
+      par <- parset3
+    } else if(fam == 2){
+      res <- array(0, dim=c(length(parset3), length(parset4), length(u1), length(u2)))
+      par <- parset3
+      par2 <- parset4
+    } else if(fam %in% c(3, 13, 23, 33)){
+      res <- array(0, dim=c(length(parset1)-1, length(u1), length(u2)))
+      par <- parset1[-1]
+    } else if(fam %in% c(4, 14, 24, 34)){
+      res <- array(0, dim=c(length(parset2), length(u1), length(u2)))
+      par <- parset2
+    } else if(fam == 5){
+      res <- array(0, dim=c(length(parset1)-1, length(u1), length(u2)))
+      par <- parset1[-1]
+    } else if(fam %in% c(6, 16, 26, 36)){
+      res <- array(0, dim=c(length(parset2)-1, length(u1), length(u2)))
+      par <- parset2[-1]
+    } else if(fam %in% c(7, 17, 27, 37, 8, 18, 28, 38)){
+      res <- array(0, dim=c(length(parset1)-1, length(parset2), length(u1), length(u2)))
+      par <- parset1[-1]
+      par2 <- parset2
+    } else if(fam %in% c(9, 19, 29, 39)){
+      res <- array(0, dim=c(length(parset2), length(parset1)-1,length(u1), length(u2)))
+      par <- parset2
+      par2 <- parset1[-1]
+    } else if(fam %in% c(10, 20, 30, 40)){
+      res <- array(0, dim=c(length(parset2), length(parset3)-1, length(u1), length(u2)))
+      par <- parset2
+      par2 <- parset3[-1]
+    } else if(fam > 100){
+      res <- array(0, dim=c(length(parset2), length(parset3), length(u1), length(u2)))
+      par <- parset2
+      par2 <- parset3
+    }
+    
+    ## length of results (depending on the parameter set)
+    n1 <- dim(res)[1]
+    n2 <- ifelse(length(dim(res))==4, dim(res)[2], 0)
+    
+    ## for rotated copulas switch sign
+    if(fam > 20 && fam < 100){
+      par <- -par
+      par2 <- -par2
+    } else if(fam %in% c(124,134,224,234)){
+      par <- -par
+    }
+    
+    ## for loops are not the best
+    iu <- 1
+    for(u in u1){
+      #iv <- 1
+      #for(v in u2){
+      uu <- rep(u,length(u2))
+        for(i in 1:n1){
+          if(n2 == 0){
+            res[i,iu,] <- do.call(what=FUN, args=list(u1=uu, u2=u2, family=fam, par=par[i], par2=0, check.pars=FALSE))
+          } else {
+            for(j in n2){
+              res[i,j,iu,] <- do.call(what=FUN, args=list(u1=uu, u2=u2, family=fam, par=par[i], par2=par2[j], check.pars=FALSE))
+            }
+          }
+        }
+        #iv <- iv + 1
+      #}
+      iu <- iu + 1
+    }
+    
+    
+    
+    ## save the results and give it the name of teh family
+    results[[k]] <- res
+    names(results)[[k]] <- as.character(fam)
+    
+    k <- k+1
+    
+  } # end familyset
+  
+  return(results)
+  
+}
