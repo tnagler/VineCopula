07313d4870702c94534cfc95b1d010b04cd85c2b
tnagler <thomas.nagler@tum.de>
2016-05-15 17:43:22 +0200

    enable new function BiCopKDE

--- R/BiCopKDE.R
+++ R/BiCopKDE.R
@@ -2,9 +2,9 @@
 #'
 #' A kernel density estimate of the copula density is visualized. The function
 #' provides the same options as \code{\link{plot.BiCop}}. Further arguments can
-#' be passed to \code{\link[kdecopula::kdecop]{kdecop}} to modify the estimate.
+#' be passed to \code{\link[kdecopula]{kdecop}} to modify the estimate.
 #'
-#' @param u1, u2 data vecotrs.
+#' @param u1,u2 numeric vectors of equal length with values in [0,1].
 #' @param type plot type; either \code{"contour"} or \code{"surface"} (partial
 #' matching is activated) for a contour or perspective/surface plot
 #' respectively.
@@ -21,17 +21,29 @@
 #' grid; default is 100 for \code{type = "contour"}, and 25 for \code{type =
 #' "surface"}.
 #' @param kde.pars list of arguments passed to
-#'  \code{\link[kdecopula::kdecop]{kdecop}}.
+#'  \code{\link[kdecopula]{kdecop}}.
 #' @param \dots optional arguments passed to \code{\link{contour}} or
 #' \code{\link{wireframe}}.
 #'
 #' @details
-#' For further details on estimation see \code{\link[kdecopula::kdecop]{kdecop}}.
+#' For further details on estimation see \code{\link[kdecopula]{kdecop}}.
 #'
 #' @author Thomas Nagler
 #'
 #' @examples
+#' # simulate data from Joe copula
+#' cop <- BiCop(3, tau = 0.3)
+#' u <- BiCopSim(1000, cop)
+#' contour(cop)  # true contours
 #'
+#' # kernel contours with standard normal margins
+#' BiCopKDE(u[, 1], u[, 2])
+#' BiCopKDE(u[, 1], u[, 2], kde.pars = list(mult = 0.5))  # undersmooth
+#' BiCopKDE(u[, 1], u[, 2], kde.pars = list(mult = 2))  # oversmooth
+#'
+#' # kernel density with uniform margins
+#' BiCopKDE(u[, 1], u[, 2], type = "surface", zlim = c(0, 4))
+#' plot(cop, zlim = c(0, 4))  # true density
 #'
 BiCopKDE <- function(u1, u2, type = "contour", margins, size,
                      kde.pars = list(), ...) {
@@ -41,20 +53,20 @@ BiCopKDE <- function(u1, u2, type = "contour", margins, size,
         udata$udata <- unname(udata$udata)
 
     ## estimate copula density with kde.pars
-    # args <- modifyList(udata, kde.pars)
-    # est <- do.call(kdecopula::kdecop, args)
-    #
-    # ## choose margins if missing
-    # if (missing(margins)) {
-    #     margins <- switch(type,
-    #                       "contour" = "norm",
-    #                       "surface" = "unif")
-    # }
-    #
-    # # plot
-    # return(plot(est,
-    #             type = type,
-    #             margins = margins,
-    #             size = size,
-    #             ...))
+    args <- modifyList(udata, kde.pars)
+    est <- do.call(kdecopula::kdecop, args)
+
+    ## choose margins if missing
+    if (missing(margins)) {
+        margins <- switch(type,
+                          "contour" = "norm",
+                          "surface" = "unif")
+    }
+
+    # plot
+    return(plot(est,
+                type = type,
+                margins = margins,
+                size = size,
+                ...))
 }
