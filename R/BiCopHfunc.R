2b901a4280bcb3faaac44336681d390abc2fb086
tnagler <thomas.nagler@tum.de>
2016-05-26 02:05:02 +0200

    adapt BiCopHfunc

--- R/BiCopHfunc.R
+++ R/BiCopHfunc.R
@@ -111,272 +111,196 @@
 #'
 #' @export BiCopHfunc
 BiCopHfunc <- function(u1, u2, family, par, par2 = 0, obj = NULL, check.pars = TRUE) {
-    ## sanity checks for u1, u2
     if (is.null(u1) == TRUE || is.null(u2) == TRUE)
         stop("u1 and/or u2 are not set or have length zero.")
     if (length(u1) != length(u2))
         stop("Lengths of 'u1' and 'u2' do not match.")
-    if (any(u1 > 1) || any(u1 < 0))
-        stop("Data has be in the interval [0,1].")
-    if (any(u2 > 1) || any(u2 < 0))
-        stop("Data has be in the interval [0,1].")
-    n <- length(u1)
 
-    ## extract family and parameters if BiCop object is provided
+    ## preprocessing of arguments
+    # store all arguments with call into a list
+    args <- as.list(environment())
+    args$call <- match.call()
+    # set dummys if family and par are missing (-> when obj is provided)
     if (missing(family))
-        family <- NA
+        args$family <- NA
     if (missing(par))
-        par <- NA
-    # for short hand usage extract obj from family
-    if (class(family) == "BiCop")
-        obj <- family
-    if (!is.null(obj)) {
-        stopifnot(class(obj) == "BiCop")
-        family <- obj$family
-        par <- obj$par
-        par2 <- obj$par2
-    }
-
-    ## adjust length for parameter vectors; stop if not matching
-    if (missing(par) & (all(family == 0)))
-        par <- 0
-    if (any(c(length(family), length(par), length(par2)) == n)) {
-        if (length(family) == 1)
-            family <- rep(family, n)
-        if (length(par) == 1)
-            par <- rep(par, n)
-        if (length(par2) == 1)
-            par2 <- rep(par2, n)
-    }
-    if (!(length(family) %in% c(1, n)))
-        stop("'family' has to be a single number or a size n vector")
-    if (!(length(par) %in% c(1, n)))
-        stop("'par' has to be a single number or a size n vector")
-    if (!(length(par2) %in% c(1, n)))
-        stop("'par2' has to be a single number or a size n vector")
-
-    ## sanity checks for family and parameters
-    if (check.pars) {
-        BiCopCheck(family, par, par2)
-    } else {
-        # allow zero parameter for Clayton an Frank otherwise
-        family[(family %in% c(3, 13, 23, 33)) & (par == 0)] <- 0
-        family[(family == 5) & (par == 0)] <- 0
-    }
+        args$par <- NA
+    # set all NA values to 0.5, but store the index (will be reset to NA)
+    args <- fix_nas(args)
+    # check if all data are in (0, 1)^2
+    check_if_01(args)
+    # extract family and parameters if BiCop object is provided
+    args <- extract_from_BiCop(args)
+    # make sure that family, par, par2 have the same length
+    args <- match_spec_lengths(args)
+    # sanity checks for family and parameters
+    args <- check_args(args)
 
     ## calculate h-functions
-    if (length(par) == 1) {
+    if (length(args$par) == 1) {
         # call for single parameters
         hfunc1 <- .C("Hfunc1",                      # h(u2|u1)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u2),
-                     as.double(u1),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u2),
+                     as.double(args$u1),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
         hfunc2 <- .C("Hfunc2",                      # h(u1|u2)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u1),
-                     as.double(u2),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u1),
+                     as.double(args$u2),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
     } else {
         # vectorized call
         hfunc1 <- .C("Hfunc1_vec",                      # h(u2|u1)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u2),
-                     as.double(u1),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u2),
+                     as.double(args$u1),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
         hfunc2 <- .C("Hfunc2_vec",                      # h(u1|u2)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u1),
-                     as.double(u2),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u1),
+                     as.double(args$u2),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
     }
 
-    ## return results
+    # reset NAs
+    hfunc1 <- reset_nas(hfunc1, args)
+    suppressWarnings(hfunc2 <- reset_nas(hfunc2, args))  # suppress second warning
+
+    # return result
     list(hfunc1 = hfunc1, hfunc2 = hfunc2)
 }
 
 
 #' @rdname BiCopHfunc
 BiCopHfunc1 <- function(u1, u2, family, par, par2 = 0, obj = NULL, check.pars = TRUE) {
-    ## sanity checks for u1, u2
     if (is.null(u1) == TRUE || is.null(u2) == TRUE)
         stop("u1 and/or u2 are not set or have length zero.")
     if (length(u1) != length(u2))
         stop("Lengths of 'u1' and 'u2' do not match.")
-    if (any(u1 > 1) || any(u1 < 0))
-        stop("Data has be in the interval [0,1].")
-    if (any(u2 > 1) || any(u2 < 0))
-        stop("Data has be in the interval [0,1].")
-    n <- length(u1)
 
-    ## extract family and parameters if BiCop object is provided
+    ## preprocessing of arguments
+    # store all arguments with call into a list
+    args <- as.list(environment())
+    args$call <- match.call()
+    # set dummys if family and par are missing (-> when obj is provided)
     if (missing(family))
-        family <- NA
+        args$family <- NA
     if (missing(par))
-        par <- NA
-    # for short hand usage extract obj from family
-    if (class(family) == "BiCop")
-        obj <- family
-    if (!is.null(obj)) {
-        stopifnot(class(obj) == "BiCop")
-        family <- obj$family
-        par <- obj$par
-        par2 <- obj$par2
-    }
+        args$par <- NA
+    # set all NA values to 0.5, but store the index (will be reset to NA)
+    args <- fix_nas(args)
+    # check if all data are in (0, 1)^2
+    check_if_01(args)
+    # extract family and parameters if BiCop object is provided
+    args <- extract_from_BiCop(args)
+    # make sure that family, par, par2 have the same length
+    args <- match_spec_lengths(args)
+    # sanity checks for family and parameters
+    args <- check_args(args)
 
-    ## adjust length for parameter vectors; stop if not matching
-    if (missing(par) & (all(family == 0)))
-        par <- 0
-    if (any(c(length(family), length(par), length(par2)) == n)) {
-        if (length(family) == 1)
-            family <- rep(family, n)
-        if (length(par) == 1)
-            par <- rep(par, n)
-        if (length(par2) == 1)
-            par2 <- rep(par2, n)
-    }
-    if (!(length(family) %in% c(1, n)))
-        stop("'family' has to be a single number or a size n vector")
-    if (!(length(par) %in% c(1, n)))
-        stop("'par' has to be a single number or a size n vector")
-    if (!(length(par2) %in% c(1, n)))
-        stop("'par2' has to be a single number or a size n vector")
-
-    ## sanity checks for family and parameters
-    if (check.pars) {
-        BiCopCheck(family, par, par2)
-    } else {
-        # allow zero parameter for Clayton an Frank otherwise
-        family[(family %in% c(3, 13, 23, 33)) & (par == 0)] <- 0
-        family[(family == 5) & (par == 0)] <- 0
-    }
-
-    ## calculate h-functions
-    if (length(par) == 1) {
+    ## calculate h-function
+    if (length(args$par) == 1) {
         # call for single parameters
         hfunc1 <- .C("Hfunc1",                      # h(u2|u1)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u2),
-                     as.double(u1),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u2),
+                     as.double(args$u1),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
     } else {
         # vectorized call
         hfunc1 <- .C("Hfunc1_vec",                      # h(u2|u1)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u2),
-                     as.double(u1),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u2),
+                     as.double(args$u1),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
     }
 
-    ## return results
+    # reset NAs
+    hfunc1 <- reset_nas(hfunc1, args)
+    # return result
     hfunc1
 }
 
 #' @rdname BiCopHfunc
 BiCopHfunc2 <- function(u1, u2, family, par, par2 = 0, obj = NULL, check.pars = TRUE) {
-    ## sanity checks for u1, u2
     if (is.null(u1) == TRUE || is.null(u2) == TRUE)
         stop("u1 and/or u2 are not set or have length zero.")
     if (length(u1) != length(u2))
         stop("Lengths of 'u1' and 'u2' do not match.")
-    if (any(u1 > 1) || any(u1 < 0))
-        stop("Data has be in the interval [0,1].")
-    if (any(u2 > 1) || any(u2 < 0))
-        stop("Data has be in the interval [0,1].")
-    n <- length(u1)
 
-    ## extract family and parameters if BiCop object is provided
+    ## preprocessing of arguments
+    # store all arguments with call into a list
+    args <- as.list(environment())
+    args$call <- match.call()
+    # set dummys if family and par are missing (-> when obj is provided)
     if (missing(family))
-        family <- NA
+        args$family <- NA
     if (missing(par))
-        par <- NA
-    # for short hand usage extract obj from family
-    if (class(family) == "BiCop")
-        obj <- family
-    if (!is.null(obj)) {
-        stopifnot(class(obj) == "BiCop")
-        family <- obj$family
-        par <- obj$par
-        par2 <- obj$par2
-    }
+        args$par <- NA
+    # set all NA values to 0.5, but store the index (will be reset to NA)
+    args <- fix_nas(args)
+    # check if all data are in (0, 1)^2
+    check_if_01(args)
+    # extract family and parameters if BiCop object is provided
+    args <- extract_from_BiCop(args)
+    # make sure that family, par, par2 have the same length
+    args <- match_spec_lengths(args)
+    # sanity checks for family and parameters
+    args <- check_args(args)
 
-    ## adjust length for parameter vectors; stop if not matching
-    if (missing(par) & (all(family == 0)))
-        par <- 0
-    if (any(c(length(family), length(par), length(par2)) == n)) {
-        if (length(family) == 1)
-            family <- rep(family, n)
-        if (length(par) == 1)
-            par <- rep(par, n)
-        if (length(par2) == 1)
-            par2 <- rep(par2, n)
-    }
-    if (!(length(family) %in% c(1, n)))
-        stop("'family' has to be a single number or a size n vector")
-    if (!(length(par) %in% c(1, n)))
-        stop("'par' has to be a single number or a size n vector")
-    if (!(length(par2) %in% c(1, n)))
-        stop("'par2' has to be a single number or a size n vector")
-
-    ## sanity checks for family and parameters
-    if (check.pars) {
-        BiCopCheck(family, par, par2)
-    } else {
-        # allow zero parameter for Clayton an Frank otherwise
-        family[(family %in% c(3, 13, 23, 33)) & (par == 0)] <- 0
-        family[(family == 5) & (par == 0)] <- 0
-    }
-
-    ## calculate h-functions
-    if (length(par) == 1) {
+    ## calculate h-function
+    if (length(args$par) == 1) {
         # call for single parameters
         hfunc2 <- .C("Hfunc2",                      # h(u1|u2)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u1),
-                     as.double(u2),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u1),
+                     as.double(args$u2),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
     } else {
         # vectorized call
         hfunc2 <- .C("Hfunc2_vec",                      # h(u1|u2)
-                     as.integer(family),
-                     as.integer(n),
-                     as.double(u1),
-                     as.double(u2),
-                     as.double(par),
-                     as.double(par2),
-                     as.double(rep(0, n)),
+                     as.integer(args$family),
+                     as.integer(args$n),
+                     as.double(args$u1),
+                     as.double(args$u2),
+                     as.double(args$par),
+                     as.double(args$par2),
+                     as.double(rep(0, args$n)),
                      PACKAGE = "VineCopula")[[7]]
     }
 
-    ## return results
+    # reset NAs
+    hfunc2 <- reset_nas(hfunc2, args)
+    # return results
     hfunc2
 }
 
