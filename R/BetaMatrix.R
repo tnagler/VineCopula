b13f86350fff9257368a17a4e2c79859df9d65e6
tnagler <thomas.nagler@tum.de>
2016-05-30 21:25:26 +0200

    NA handling for BetaMatrix

--- R/BetaMatrix.R
+++ R/BetaMatrix.R
@@ -1,29 +1,40 @@
 #' Matrix of Empirical Blomqvist's Beta Values
-#' 
+#'
 #' This function computes the empirical Blomqvist's beta.
-#' 
-#' 
+#'
+#'
 #' @param data An N x d data matrix.
+#'
 #' @return Matrix of the empirical Blomqvist's betas.
+#'
 #' @author Ulf Schepsmeier
+#'
 #' @seealso \code{\link{TauMatrix}}, \code{\link{BiCopPar2Beta}},
 #' \code{\link{RVinePar2Beta}}
+#'
 #' @references Blomqvist, N. (1950).  On a measure of dependence between two
 #' random variables. The Annals of Mathematical Statistics, 21(4), 593-600.
-#' 
+#'
 #' Nelsen, R. (2006). An introduction to copulas.  Springer
 #' @examples
-#' 
+#'
 #' data(daxreturns)
-#' Data <- as.matrix(daxreturns)
-#' 
+#' data <- as.matrix(daxreturns)
+#'
 #' # compute the empirical Blomqvist's betas
-#' BetaMatrix(Data)
-#' 
-#' @export BetaMatrix
+#' BetaMatrix(data)
+#'
 BetaMatrix <- function(data) {
+    ## preprocessing of arguments
+    args <- preproc(c(as.list(environment()), call = match.call()),
+                    check_data,
+                    remove_nas,
+                    check_nobs,
+                    na.txt = " Only complete observations are used.")
+    list2env(args, environment())
+
     d <- dim(data)[2]
-    
+
     betahat <- matrix(1, d, d)
     for (i in 1:(d - 1)) {
         u1 <- data[, i]
@@ -33,7 +44,7 @@ BetaMatrix <- function(data) {
             betahat[j, i] <- betahat[i, j]
         }
     }
-    
+
     return(betahat)
 }
 
