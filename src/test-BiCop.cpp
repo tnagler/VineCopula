ed7ac17a06bd143c08bf718872083ddaaceb9560
tnagler <thomas.nagler@tum.de>
2016-12-07 10:21:33 +0100

    enable testthat

---
+++ src/test-BiCop.cpp
@@ -0,0 +1,33 @@
+#include "include/BiCop.hpp"
+
+// functions for unit testing in R
+extern "C" {
+    void test_bicop(int* family, double* par, double* par2) {
+        BiCop obj(*family, *par, *par2);
+        if (*family == -1)
+            obj = BiCop();
+        *family = obj.getFamily();
+        *par = obj.getPar();
+        *par2 = obj.getPar2();
+    }
+}
+extern "C" {
+    void test_bicop_set(int* family, double* par, double* par2) {
+        BiCop obj;
+        obj.setFamily(*family);
+        obj.setPar(*par);
+        obj.setPar2(*par2);
+    }
+}
+extern "C" {
+    void test_bicop_hfunc1(double* u1, double* u2, double* out, int* n) {
+        BiCop obj(1, 0.5, 0.0);
+        obj.hFunc1(u1, u2, out, n);
+    }
+}
+extern "C" {
+    void test_bicop_hfunc2(double* u1, double* u2, double* out, int* n) {
+        BiCop obj(1, 0.5, 0.0);
+        obj.hFunc2(u1, u2, out, n);
+    }
+}
---
+++ tests/testthat/test-BiCop.R
@@ -0,0 +1,61 @@
+context("C++ class BiCop")
+
+call_bicops <- function(fun_name, family, par, par2) {
+    .C(fun_name,
+       family = as.integer(family),
+       par = as.double(par),
+       par2 = as.double(par2),
+       PACKAGE = "VineCopula")
+}
+
+test_that("default constructor works", {
+    obj <- call_bicops("test_bicop", -1, 234, 2341)
+    expect_equal(obj$family, 0)
+    expect_equal(obj$par, 0)
+    expect_equal(obj$par2, 0)
+})
+
+test_that("parametrized constructor works", {
+    obj <- call_bicops("test_bicop", 1, 0.3, 7.0)
+    expect_equal(obj$family, 1)
+    expect_equal(obj$par, 0.3)
+    expect_equal(obj$par2, 7.0)
+})
+
+test_that("setter works", {
+    obj <- call_bicops("test_bicop_set", 1, 0.3, 7.0)
+    expect_equal(obj$family, 1)
+    expect_equal(obj$par, 0.3)
+    expect_equal(obj$par2, 7.0)
+})
+
+call_bicop_funcs <- function(fun_name, u1, u2, n) {
+    .C(fun_name,
+       u1 = as.double(u1),
+       u2 = as.double(u2),
+       out = as.double(rep(0, n)),
+       n = as.integer(n),
+       PACKAGE = "VineCopula")
+}
+
+test_that("hfunc1 works", {
+    obj <- call_bicop_funcs("test_bicop_hfunc1", runif(5), runif(5), 5)
+    expect_equal(obj$u1, obj$out)
+    expect_length(obj$u1, 5)
+    expect_length(obj$u2, 5)
+    expect_length(obj$out, 5)
+})
+
+
+library(VineCopula)
+
+test_that("hfunc2 works", {
+    n <- 10
+    u1 <- runif(n)
+    u2 <- runif(n)
+    obj <- call_bicop_funcs("test_bicop_hfunc2", u1, u2, n)
+    expect_equal(obj$out, BiCopHfunc2(u2, u1, 1, 0.5))
+    expect_length(obj$u1, n)
+    expect_length(obj$u2, n)
+    expect_length(obj$out, n)
+})
