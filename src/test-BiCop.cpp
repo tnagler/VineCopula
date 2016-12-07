6937a348199ef812be7396cc619316a34f038f34
tnagler <thomas.nagler@tum.de>
2016-12-07 11:43:06 +0100

    adapt test scripts to actual c++ names

--- src/test-BiCop.cpp
+++ src/test-BiCop.cpp
@@ -2,7 +2,7 @@
 
 // functions for unit testing in R
 extern "C" {
-    void test_bicop(int* family, double* par, double* par2) {
+    void test_BiCop(int* family, double* par, double* par2) {
         BiCop obj(*family, *par, *par2);
         if (*family == -1)
             obj = BiCop();
@@ -12,7 +12,7 @@ extern "C" {
     }
 }
 extern "C" {
-    void test_bicop_set(int* family, double* par, double* par2) {
+    void test_BiCop_set(int* family, double* par, double* par2) {
         BiCop obj;
         obj.setFamily(*family);
         obj.setPar(*par);
@@ -20,21 +20,21 @@ extern "C" {
     }
 }
 extern "C" {
-    void test_bicop_hfunc1(double* u1, double* u2, double* out, int* n,
+    void test_BiCop_hFunc1(double* u1, double* u2, double* out, int* n,
                            int* family, double* par, double* par2) {
         BiCop obj(*family, *par, *par2);
         obj.hFunc1(u1, u2, out, n);
     }
 }
 extern "C" {
-    void test_bicop_hfunc2(double* u1, double* u2, double* out, int* n,
+    void test_BiCop_hFunc2(double* u1, double* u2, double* out, int* n,
                            int* family, double* par, double* par2) {
         BiCop obj(*family, *par, *par2);
         obj.hFunc2(u1, u2, out, n);
     }
 }
 extern "C" {
-    void test_bicop_pdf(double* u1, double* u2, double* out, int* n,
+    void test_BiCop_PDF(double* u1, double* u2, double* out, int* n,
                         int* family, double* par, double* par2) {
         BiCop obj(*family, *par, *par2);
         obj.PDF(u1, u2, out, n);
@@ -42,7 +42,7 @@ extern "C" {
 }
 
 extern "C" {
-    void test_bicop_loglik(double* u1, double* u2, double* out, int* n,
+    void test_BiCop_logLik(double* u1, double* u2, double* out, int* n,
                            int* family, double* par, double* par2) {
         BiCop obj(*family, *par, *par2);
         obj.logLik(u1, u2, out, n);
--- tests/testthat/test-BiCop.R
+++ tests/testthat/test-BiCop.R
@@ -1,6 +1,6 @@
 context("C++ class BiCop")
 
-call_bicops <- function(fun_name, family, par, par2) {
+call_BiCops <- function(fun_name, family, par, par2) {
     .C(fun_name,
        family = as.integer(family),
        par = as.double(par),
@@ -9,37 +9,25 @@ call_bicops <- function(fun_name, family, par, par2) {
 }
 
 test_that("default constructor works", {
-    obj <- call_bicops("test_bicop", -1, 234, 2341)
+    obj <- call_BiCops("test_BiCop", -1, 234, 2341)
     expect_equal(obj$family, 0)
     expect_equal(obj$par, 0)
     expect_equal(obj$par2, 0)
 })
 
 test_that("parametrized constructor works", {
-    obj <- call_bicops("test_bicop", 1, 0.3, 7.0)
+    obj <- call_BiCops("test_BiCop", 1, 0.3, 7.0)
     expect_equal(obj$family, 1)
     expect_equal(obj$par, 0.3)
     expect_equal(obj$par2, 7.0)
 })
 
 test_that("setter works", {
-    obj <- call_bicops("test_bicop_set", 1, 0.3, 7.0)
+    obj <- call_BiCops("test_BiCop_set", 1, 0.3, 7.0)
     expect_equal(obj$family, 1)
     expect_equal(obj$par, 0.3)
     expect_equal(obj$par2, 7.0)
 })
-
-call_bicop_funcs <- function(fun_name, u1, u2, n, family, par, par2) {
-    .C(fun_name,
-       u1 = as.double(u1),
-       u2 = as.double(u2),
-       out = as.double(rep(0, n)),
-       n = as.integer(n),
-       family = as.integer(family),
-       par = as.double(par),
-       par2 = as.double(par2),
-       PACKAGE = "VineCopula")
-}
 
 library(VineCopula)
 n <- 10
@@ -51,23 +39,34 @@ u2 <- runif(n)
 family <- 23
 par <- -1
 par2 <- 0
+call_BiCop_funcs <- function(fun_name, u1, u2, n, family, par, par2) {
+    .C(fun_name,
+       u1 = as.double(u1),
+       u2 = as.double(u2),
+       out = as.double(rep(0, n)),
+       n = as.integer(n),
+       family = as.integer(family),
+       par = as.double(par),
+       par2 = as.double(par2),
+       PACKAGE = "VineCopula")
+}
 
-test_that("hfunc1 works", {
-    obj <- call_bicop_funcs("test_bicop_hfunc1", u1, u2, n, family, par, par2)
+test_that("hFunc1 works", {
+    obj <- call_BiCop_funcs("test_BiCop_hFunc1", u1, u2, n, family, par, par2)
     expect_equal(obj$out, BiCopHfunc1(u1, u2, family, par))
 })
 
-test_that("hfunc2 works", {
-    obj <- call_bicop_funcs("test_bicop_hfunc2", u1, u2, n, family, par, par2)
+test_that("hFunc2 works", {
+    obj <- call_BiCop_funcs("test_BiCop_hFunc2", u1, u2, n, family, par, par2)
     expect_equal(obj$out, BiCopHfunc2(u1, u2, family, par))
 })
 
 test_that("PDF works", {
-    obj <- call_bicop_funcs("test_bicop_pdf", u1, u2, n, family, par, par2)
+    obj <- call_BiCop_funcs("test_BiCop_PDF", u1, u2, n, family, par, par2)
     expect_equal(obj$out, BiCopPDF(u1, u2, family, par))
 })
 
 test_that("logLik works", {
-    obj <- call_bicop_funcs("test_bicop_loglik", u1, u2, n, family, par, par2)
+    obj <- call_BiCop_funcs("test_BiCop_logLik", u1, u2, n, family, par, par2)
     expect_equal(obj$out[1], sum(log(BiCopPDF(u1, u2, family, par))))
 })
